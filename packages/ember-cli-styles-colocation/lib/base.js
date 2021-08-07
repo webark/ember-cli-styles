92d44d4ca4e494d1c423f4d2216d1ea71aa3d9b9
Mark David Avery <mark@webark.cc>
2021-08-07 08:41:04 -0700

    fix: colocation director was werid

---
+++ packages/ember-cli-styles-colocation/lib/base.js
@@ -0,0 +1,29 @@
+const funnel = require('broccoli-funnel');
+
+module.exports = class Base {
+  constructor(options) {
+    this.getCssExtentions = options.getCssExtentions;
+    this.baseName = options.baseName;
+    this.terseClassNames = options.terseClassNames;
+  }
+
+  get extentions() {
+    return this.getCssExtentions();
+  }
+
+  colocatedStyles(tree, srcDir = this.baseName, destDir = this.baseName) {
+    const baseFiles = funnel(tree, {
+      srcDir,
+      destDir,
+      allowEmpty: true,
+      annotation: 'Funnel (ember-component-css grab files addon style files)',
+    });
+
+    return funnel(baseFiles, {
+      include: [`**/*.{${this.extentions},}`],
+      exclude: [`**/styles/**/*`],
+      allowEmpty: true,
+      annotation: 'Funnel (ember-component-css grab files addon style files)',
+    });
+  }
+}
---
+++ packages/ember-cli-styles-colocation/lib/colocate.js
@@ -0,0 +1,54 @@
+const funnel = require('broccoli-funnel');
+const merge = require('broccoli-merge-trees');
+const manifest = require('broccoli-file-manifest');
+
+const Base = require('./base.js');
+
+module.exports.MoveAddonColocatedStyles = class MoveAddonColocatedStyles extends Base {
+  constructor(options) {
+    super(options);
+    this.addonRealDir = options.addonRealDir;
+  }
+
+  get name() {
+    return 'move-addon-colocated-styles';
+  }
+
+  toTree(tree) {
+    const colocatedStyles = this.colocatedStyles(this.addonRealDir, '');
+    
+    return merge([tree, colocatedStyles], { overwrite: true });
+  }
+}
+
+const MANIFEST_TEMPATES = {
+  default: '@import "<file-path>";',
+  sass: '@import "<file-path>"',
+  styl: '@import "<file-path>"',
+  css: '@import "../../<file-path>";',
+}
+
+module.exports.ColocateStyles = class ColocateStyles extends Base {
+  get name() {
+    return 'colocate-styles';
+  }
+
+  generateManifest(tree, destDir) {
+    const manifesto = manifest(tree, {
+      outputFileNameWithoutExtension: 'ember-styles',
+      templates: MANIFEST_TEMPATES,
+      annotation: 'Manifest (ember-component-css style file manifest)',
+    });
+
+    return funnel(manifesto, {
+      destDir,
+    });
+  }
+
+  toTree(tree, destDir) {
+    const colocatedStyles = this.colocatedStyles(tree);
+    const manifests = this.generateManifest(colocatedStyles, destDir);
+    
+    return merge([tree, manifests], { overwrite: true });
+  }
+}
